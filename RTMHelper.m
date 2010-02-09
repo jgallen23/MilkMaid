87f1df1ac6f5140dcd90f402991c7b843624c60f
Greg Allen <mail@jga23.com>
2010-02-09 07:49:37 -0800

    Order by due date first

--- RTMHelper.m
+++ RTMHelper.m
@@ -9,13 +9,67 @@
 #import "RTMHelper.h"
 #import "NSDateHelper.h"
 
-static int compare (id obj1, id obj2, void *context) {
-	if ([[obj1 objectForKey:@"due"] isEqual:[obj2 objectForKey:@"due"]]) {
-		return [[obj1 objectForKey:@"priority"] compare:[obj2 objectForKey:@"priority"]];
-	} else {
-		return [[obj1 objectForKey:@"due"] compare:[obj2 objectForKey:@"due"]];
+
+
+static int comparePriorities(id obj1, id obj2, void *context) {
+	NSComparisonResult comp = 0;
+	int priority1 = [[obj1 objectForKey:@"priority"] intValue];
+	int priority2 = [[obj2 objectForKey:@"priority"] intValue];
+	
+	if (priority1 == 0) {
+		priority1 = 4;
+	}
+	if (priority2 == 0) {
+		priority2 = 4;
 	}
 
+
+	if (priority1 > priority2) {
+		comp = NSOrderedDescending;
+	} else if (priority2 > priority1) {
+		comp = NSOrderedAscending;
+	} else {
+		comp = NSOrderedSame;
+	}
+	
+	//NSLog(@"%@ (%d) v %@ (%d) = %d", [obj1 objectForKey:@"name"], priority1, [obj2 objectForKey:@"name"], priority2, comp);
+	return comp;
+}
+
+static int compareDates(id obj1, id obj2, void *context) {
+	
+	id due1 = [obj1 objectForKey:@"due"];
+	id due2 = [obj2 objectForKey:@"due"];
+	
+	NSComparisonResult comp = 0;
+	
+	if ([due1 isKindOfClass:[NSDate class]] && [due2 isKindOfClass:[NSDate class]]) {
+		comp = [due1 compare:due2];
+	} else if ([due1 isKindOfClass:[NSDate class]] && ![due2 isKindOfClass:[NSDate class]]) {
+		comp = NSOrderedAscending;
+	} else if ([due2 isKindOfClass:[NSDate class]]) {
+		comp = NSOrderedDescending;
+	} else {
+		comp = NSOrderedSame;
+	}
+	//NSLog(@"%@ (%d) v %@ (%d) = %d", [obj1 objectForKey:@"name"], due1, [obj2 objectForKey:@"name"], due2, comp);
+	return comp;
+}
+
+static int compareNames(id obj1, id obj2, void *context) {
+	NSString *name1 = [obj1 objectForKey:@"name"];
+	NSString *name2 = [obj2 objectForKey:@"name"];
+	NSComparisonResult comp = [name1 compare:name2];
+	NSLog(@"%@ v %@  = %d", name1, name2, comp);
+	return comp;
+}
+
+static int compare(id obj1, id obj2, void *context) {
+	NSComparisonResult comp = compareDates(obj1, obj2, &context);
+	if (comp == NSOrderedSame) {
+		comp = comparePriorities(obj1, obj2, &context);
+	}
+	return comp;
 }
 
 @implementation RTMHelper
